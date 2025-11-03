3d16a33b54af5c57ec113aa7a8c54616bbce7a29
yk-graph <yokokura.tatsuya@gmail.com>
2025-11-02 20:34:43 -0800

    add: prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import { ModeToggle } from '@/components/buttons';
-
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <ModeToggle />
-    </div>
-  );
+  return <div className="flex min-h-screen items-center justify-center">hello world</div>;
 }
