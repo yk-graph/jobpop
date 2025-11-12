2c25db5adf3bb6851b4a94ac075e740f28e74474
yk-graph <yokokura.tatsuya@gmail.com>
2025-11-11 16:52:49 -0800

    format

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,4 +1,5 @@
 export * from './auth/get-user-by-email';
 export * from './auth/google-authenticate';
+export * from './auth/instagram-authenticate';
 export * from './auth/login';
 export * from './auth/register';
--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -1,3 +1,4 @@
 export * from './google-button';
 export * from './icon-button';
+export * from './instagram-button';
 export * from './mode-toggle';
